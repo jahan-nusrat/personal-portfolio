d34ae2d456bc0ad94c6d050396501da5027d6314
Nusrat Jahan <njnusrat53@gmail.com>
2020-11-02 00:20:39 +0600

    bug fixed

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,20 @@
 export const frontend=['JavaScript (ES6+)','ReactJS','Redux','HTML5','CSS3','Bootstrap','SCSS','Material-UI']
 export const backend=['NodeJS', 'ExpressJS', 'MongoDB','Mongoose']
 export const tools=['Git', 'GitHub', 'VS Code', 'Heroku', 'Netlify', 'Firebase', 'Chrome Dev Tools']
+export const projects= [
+    {
+        title:'Creative Agency',
+        overview:'A full-stack web app where user can select service, order that service, as well as give review to a specific service. An admin can review all the orders, change the order status of a service, and add new orders dynamically. Also, admin can set a new email as admin.',
+        img: 'https://i.ibb.co/3sXJ3f1/project1.png'
+    },
+    {
+        title:'Volunteer Network',
+        overview:'A full-stack app where, if user wants to join a volunteer activity of his choice, he has to register first.User will be able to see if any previous volunteer work has been registered on his dashboard.Admin can see all the volunteers in his dashboard and delete any volunteer if he wants.',
+        img: 'https://i.ibb.co/syv0yS1/project2.png'
+    },
+    {
+        title:'Red Onion Restaurant',
+        overview:'Built an SPA where anyone can order their favorite food from the menu and have the option of breakfast, lunch and dinner.Users can order food and pay online to receive food while sitting at home.',
+        img: 'https://i.ibb.co/s63VkGD/project3.png'
+    },
+]
